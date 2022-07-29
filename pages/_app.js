9a5fc8babf2151598e4d9f07f8c74b549c709306
lvidal1 <lvidal910@gmail.com>
2022-07-29 10:53:41 -0500

    [Component] Add Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@material-tailwind/react';
 import { MATERIALS_TAILWIND_THEME } from '../config/material';
+import Layout from '../components/Layout/Layout';
 
 function MyApp({ Component, pageProps }) {
 
   return <ThemeProvider value={MATERIALS_TAILWIND_THEME}>
-    <Component {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </ThemeProvider>
 }
 
