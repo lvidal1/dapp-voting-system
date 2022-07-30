0788d0928f697d702b91d4a8856013762172a429
lvidal1 <lvidal910@gmail.com>
2022-07-30 15:27:05 -0500

    [Component] Add Navbar component

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -1,8 +1,24 @@
+import { Navbar, Button, Typography } from "@material-tailwind/react";
 import React from "react";
 
 const Header = () => {
     return (
         <header>
+            <Navbar className="mx-auto max-w-screen-xl bg-white">
+                <div className="text-blue-gray-900 container flex items-center justify-between">
+                    <Typography
+                        as="a"
+                        href="#"
+                        variant="small"
+                        className="mr-4 cursor-pointer py-1.5 font-normal"
+                    >
+                        Election 2022
+                    </Typography>
+                    <Button variant="gradient" size="sm">
+                        Vote
+                    </Button>
+                </div>
+            </Navbar>
         </header>
     )
 }
